484a42c5e4f87d563f142bb438c273812780dca4
Solaris5 <yu.heydemann@yahoo.de>
2020-11-04 22:13:59 +0100

    Update 00-database.sql

--- graphile/db/init/00-database.sql
+++ graphile/db/init/00-database.sql
@@ -2,8 +2,22 @@
 
 CREATE TABLE public.component (
     //TODO: add attributes 
+    id integer NOT NULL,
+    name text ,
+    version text,
+    packagen text ,
+    lincese text ,
+    CONSTRAINT component_pkey PRIMARY KEY (id);
 );
 
 CREATE TABLE public.dependency (
     //TODO add linking attribute
+    ancestor_id integer NOT NULL,
+    descendant_id integer NOT NULL,
+    depth integer,
+    name text ,
+    linking text ,
+    CONSTRAINT pr_depencecy PRIMARY KEY (ancestor_id)
+        INCLUDE(ancestor_id, descendant_id);
 );
+
