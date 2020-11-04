0312a3e12d33a4c668b8639757a642bb69cc397c
= <=>
2020-11-04 12:14:06 +0100

    Vartiante1

---
+++ Adjacency list/01-data.sql
@@ -0,0 +1,36 @@
+INSERT INTO public."Alist"(
+	id, name, parent_id)
+	VALUES (1,'A',NULL);
+
+INSERT INTO public."Alist"(
+	id, name, parent_id)
+	VALUES (2,'B',1);
+
+INSERT INTO public."Alist"(
+	id, name, parent_id)
+	VALUES (6,'C',1);
+
+INSERT INTO public."Alist"(
+	id, name, parent_id)
+	VALUES  (3,'D',2);
+
+INSERT INTO public."Alist"(
+	id, name, parent_id)
+	VALUES (4,'E',2);
+
+INSERT INTO public."Alist"(
+	id, name, parent_id)
+	VALUES (5,'F',2);
+INSERT INTO public."Alist"(
+	id, name, parent_id)
+	VALUES (7,'H',6);
+
+INSERT INTO public."Alist"(
+	id, name, parent_id)
+	VALUES (8,'K',7);
+INSERT INTO public."Alist"(
+	id, name, parent_id)
+	VALUES  (9,'I',6);
+INSERT INTO public."Alist"(
+	id, name, parent_id)
+	VALUES (10,'J',6);
