3fae0fd4b206296d88a6db641a2f063d5eb9a71e
Andreas Bauer <dev@andreasbauer.org>
2020-11-04 19:54:56 +0100

    Add basic Graphile and Postgres structure

---
+++ graphile/db/init/00-database.sql
@@ -0,0 +1,9 @@
+\connect product_model;
+
+CREATE TABLE public.component (
+    //TODO: add attributes 
+);
+
+CREATE TABLE public.dependency (
+    //TODO add linking attribute
+);
---
+++ graphile/db/init/01-data.sql
@@ -0,0 +1,9 @@
+\connect product_model;
+
+/*Create some example components*/
+INSERT INTO public.component ...
+//TODO: complete insert statement
+
+/*Create some dependencies between components*/
+INSERT INTO public.dependency ... 
+//TODO: complete insert statement
