f82a266ac3f65c42802f246637835199ad83c050
= <=>
2020-11-05 22:37:10 +0100

    closure table

--- graphile/db/init/00-database.sql
+++ graphile/db/init/00-database.sql
@@ -1,17 +1,17 @@
 \connect product_model;
 
 CREATE TABLE public.component (
-    //TODO: add attributes 
+
     id integer NOT NULL,
     name text ,
     version text,
-    packagen text ,
+    package text,
     lincese text ,
     CONSTRAINT component_pkey PRIMARY KEY (id);
 );
 
 CREATE TABLE public.dependency (
-    //TODO add linking attribute
+ 
     ancestor_id integer NOT NULL,
     descendant_id integer NOT NULL,
     depth integer,
