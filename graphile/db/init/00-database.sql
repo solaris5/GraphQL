abc3b02480968e2913ce5cc42b5f7fa55c4ae646
momo <yu.heydemann@yahoo.de>
2020-11-07 16:47:39 +0100

    1

--- graphile/db/init/00-database.sql
+++ graphile/db/init/00-database.sql
@@ -12,6 +12,8 @@ create table public.dependency(
   ancestor_id integer not null,
   descendant_id integer not null,
   depth integer not null,
+  name text,
+  linking text,
   primary key (ancestor_id,descendant_id),
   foreign key (ancestor_id) references public.component(id),
   foreign key (descendant_id) references public.component(id)
